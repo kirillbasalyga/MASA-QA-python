9d0b9f49f1d1e89c0811a212564e0203762ba240
kirillbasalyga <kirillbasalyga@gmail.com>
2026-01-03 19:43:42 +0200

    Homework lesson 5

--- Home work lesson 5/Task 1.py
+++ Home work lesson 5/Task 1.py
@@ -1,18 +1,19 @@
-Anna = {
-    "name": "Анна Иванова",
-    "phone": "+79001234567",
-    "email": "anna.ivanova@example.com"
+contacts = {
+    "Anna" : {
+        "name": "Анна Иванова",
+        "phone": "+79001234567",
+        "email": "anna.ivanova@example.com"
+    },
+    "Petr":{
+        "name": "Петр Сидоров",
+        "phone": "+79119876543",
+        "email": "petr.sidorov@example.com"
+    }
 }
 
-Petr = {
-    "name": "Петр Сидоров",
-    "phone": "+79119876543",
-    "email": "petr.sidorov@example.com"
-}
 
-print(Anna)
-Petr["phone"] = "+79225551122"
-Anna["address"] = "г.Москва, ул. Пушкина, д. 10"
-del Petr["email"]
-
-print(f"{Anna}, \n{Petr}")
+print(contacts["Anna"])
+contacts["Petr"]["phone"] = "+79225551122"
+contacts["Anna"]["address"] = "г.Москва, ул. Пушкина, д. 10"
+del contacts["Petr"]["email"]
+print(contacts)
---
+++ Home work lesson 5/Task 3.py
@@ -0,0 +1,9 @@
+numbers_data = (15, 22, 10, 30, 5, 25, 18)
+summa = 0
+for i in range(7):
+    summa += (numbers_data[i])
+
+print(f"Сумма всех элементов = {summa}")
+print(f"Среднее арифметическое = {summa / 7}")
+print(f"Максимальное знаечение: {max(numbers_data)}")
+print(f"Минимальное знаечение: {min(numbers_data)}")
